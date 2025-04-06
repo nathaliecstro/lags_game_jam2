26fa07e9615a994b8a173109cefb5308edcd1436
KennethSepu1 <124213573+KennethSepu1@users.noreply.github.com>
2025-04-06 00:48:23 -0400

    Hud and character change

--- updated_lags_game_jam/SCENES/main.gd
+++ updated_lags_game_jam/SCENES/main.gd
@@ -5,15 +5,18 @@ extends Node
 
 var score
 
-func game_over():
-	$ScoreTimer.stop()
-	$FoodTimer.stop()
 
 func new_game():
 	score = 0
-	$CharacterBody2D.start($StartPos.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$Sapo.start($StartPos.position)
+	$HUD.show_message("Get Ready")
 	
+func game_over():
+	$ScoreTimer.stop()
+	$FoodTimer.stop()
+	$HUD.show_game_over()
 	
 func _on_food_timer_timeout():
 	# Randomly pick a food scene
@@ -45,11 +48,11 @@ func _on_food_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
-
+	$HUD.update_score(score)
+	
 func _on_start_timer_timeout():
 	$FoodTimer.start()
 	$ScoreTimer.start()
 
 func _ready():
-	new_game()
-	
+	pass
